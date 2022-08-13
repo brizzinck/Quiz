7ef036ed3d512deb0b069633765ea4b8e439029d
Vlad <lavrishkovlad@gmail.com>
2022-08-13 23:13:58 +0300

    Correct Git

---
+++ Assets/Scripts/Cell/Cell.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+public sealed class Cell : MonoBehaviour
+{
+    [SerializeField] private Image _imageCell;
+    private string _identifierCell;
+    private StartLevel _startScen;
+    private AnimationController _animationController;
+    private LevelTransition _levelTransition;
+
+    public string IdentifierCell { get => _identifierCell; set => _identifierCell = value; }
+    public Image ImageCell { get => _imageCell; }
+
+    public void Choose()
+    {
+        Sequence sequence = DOTween.Sequence();
+        if (_startScen.TrueNameCell == _identifierCell)
+        {
+            _animationController.Bounce(_imageCell.gameObject, sequence);
+            sequence.OnComplete(_levelTransition.TransitionNextLevel);
+        }
+        else
+        {
+            _animationController.EaseInBounce(_imageCell.gameObject, sequence);
+        }
+    }
+
+    private void Awake()
+    {
+        _startScen = FindObjectOfType<StartLevel>();
+        _animationController = FindObjectOfType<AnimationController>();
+        _levelTransition = FindObjectOfType<LevelTransition>();
+    }
+}
+
